e040c6fda9824027c99c90c7f59a0c2c96049094
esc <esc@youhavethewrong.info>
2015-01-27 10:23:21 -0600

    initial

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject search "0.1.0-SNAPSHOT"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+    :dependencies [[org.clojure/clojure "1.6.0"]])
